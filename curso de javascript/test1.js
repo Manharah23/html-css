67b06bc3e3a2c11e8057f4ef30436f539e387895
Francisco Magure <magure9323@gmail.com>
2024-03-06 01:18:16 +0200

    ggggg

---
+++ curso de javascript/test1.js
@@ -0,0 +1,7 @@
+var vel = 12.0
+console.log(`A velocidade do seu carro e ${vel}km/h`)
+if (vel > 60.0){
+    console.log(`voce ultrapassou a velocidade maxima permitida nessa via`)
+}
+    console.log(`dentro do padrao estabelecido`)
+    
---
+++ curso de javascript/test2.js
@@ -0,0 +1,8 @@
+var pais = 'mocambique'
+console.log(`vivendo em ${pais}`)
+if (pais !='mocambique' ){
+    console.log(`voce e mocambicano`)
+ }
+  else {
+        console.log(`voce e estrangeiro`)
+    }
