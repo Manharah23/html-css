3900e6a17078eca0a91996a6f220fd8dc0477e58
Francisco Magure <magure9323@gmail.com>
2024-03-25 21:24:22 +0300

    ww

--- curso de javascript/exercicios de js/climaagora/clima.js
+++ curso de javascript/exercicios de js/climaagora/clima.js
@@ -3,19 +3,20 @@ let apikey = "83893b62391f6e4bd46977c3f09b5c0f"
 
 let apiCountryURL ="https://countryflagsapi.com/png/"
 
-let cityinput = document.querySelector("#city-input")
-let searchBtn = document.querySelector("#search")
+const cityinput = document.getElementById("#city-input");
+const searchbtn = document.getElementById("#search");
+
 
 
 /*funcoes*/
-let mostrarclimadacidade = (city) => {
-    console.log(city)}
+
+
+
 /*eventos*/
 
-searchBtn.addEventListener('click', (e) => {
+searchbtn.addEventListener("click", (e) =>{
     e.preventDefault();
 
-    let city = cityinput.value;
-
+    const city =  cityinput.value;
     console.log("city")
-});
+} )
